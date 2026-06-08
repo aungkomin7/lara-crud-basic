5fe6511d533fae72dd5752845e2fe588ef9b4b8d
aungkomin7 <winfloweraungkomin1997@gmail.com>
2026-06-08 14:25:09 +0700

    deteted PostController file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 
-use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,10 +8,4 @@ Route::get('/', function () {
 });
 
 
-Route::get("/posts",[PostController::class,"index"])->name("post.index");
-Route::get("/create",[PostController::class,"create"])->name("post.create");
-Route::post("/store",[PostController::class,"store"])->name("post.store");
-Route::get("/show/{id}",[PostController::class,"show"])->name("post.show");
-Route::delete("/destroy/{id}",[PostController::class,"destroy"])->name("post.destroy");
-Route::get("/edit/{id}",[PostController::class,"edit"])->name("post.edit");
-Route::put("/update/{id}",[PostController::class,"update"])->name("post.update");
+
